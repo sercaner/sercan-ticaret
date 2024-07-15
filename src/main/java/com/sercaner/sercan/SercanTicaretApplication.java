be82bed775e9635ea5f1495bd3c1670bcec0cfd9
Sercan Er <sercaner.221@gmail.com>
2024-07-15 19:08:42 +0300

    project created

---
+++ src/main/java/com/sercaner/sercan/SercanTicaretApplication.java
@@ -0,0 +1,13 @@
+package com.sercaner.sercan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SercanTicaretApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SercanTicaretApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sercaner/sercan/SercanTicaretApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sercaner.sercan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SercanTicaretApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
