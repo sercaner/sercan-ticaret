71f9f8d7253a4fb3105cde57f13b410d2d761978
Sercan Er <sercaner.221@gmail.com>
2024-07-16 22:54:51 +0300

    abstract

--- src/main/java/com/sercaner/sercan/model/BaseEntity.java
+++ src/main/java/com/sercaner/sercan/model/BaseEntity.java
@@ -10,7 +10,7 @@ import jakarta.persistence.PreUpdate;
 import java.time.LocalDateTime;
 
 @MappedSuperclass
-public class BaseEntity {
+public abstract class BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
